4f08d8f4d39da1dae5624e0a9acec917438b38fd
minerdev <minerdev2020@gmail.com>
2021-01-22 13:20:20 +0900

    상세정보 화면 덜 고친 코드 고침.

--- app/src/main/java/com/minerdev/greformanager/HouseDetailActivity.java
+++ app/src/main/java/com/minerdev/greformanager/HouseDetailActivity.java
@@ -20,39 +20,14 @@ public class HouseDetailActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_house_detail);
 
-        Toolbar toolbar = findViewById(R.id.house_detail__toolbar);
+        Toolbar toolbar = findViewById(R.id.house_detail_toolbar);
         setSupportActionBar(toolbar);
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         MenuInflater menuInflater = getMenuInflater();
-        menuInflater.inflate(R.menu.main_toolbar, menu);
-
-        SearchView searchView = findViewById(R.id.main_searchView);
-        searchView.setVisibility(View.VISIBLE);
-        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
-            @Override
-            public boolean onQueryTextSubmit(String query) {
-//                rearrangeList(query, group1, group2);
-                InputMethodManager manager = (InputMethodManager) getSystemService(INPUT_METHOD_SERVICE);
-                manager.hideSoftInputFromWindow(getCurrentFocus().getWindowToken(), InputMethodManager.HIDE_NOT_ALWAYS);
-                return true;
-            }
-
-            @Override
-            public boolean onQueryTextChange(String newText) {
-                return true;
-            }
-        });
-        searchView.setOnCloseListener(new SearchView.OnCloseListener() {
-            @Override
-            public boolean onClose() {
-                searchView.onActionViewCollapsed();
-                return true;
-            }
-        });
-
+        menuInflater.inflate(R.menu.house_detail_toolbar, menu);
         return super.onCreateOptionsMenu(menu);
     }
 
