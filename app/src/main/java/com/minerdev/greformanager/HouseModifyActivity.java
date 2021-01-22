f13a316209272e8c14e537620d04cbabbea83d9b
minerdev <minerdev2020@gmail.com>
2021-01-22 11:56:28 +0900

    뒤로가기 버튼 활성화.

--- app/src/main/java/com/minerdev/greformanager/HouseModifyActivity.java
+++ app/src/main/java/com/minerdev/greformanager/HouseModifyActivity.java
@@ -1,8 +1,11 @@
 package com.minerdev.greformanager;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
 public class HouseModifyActivity extends AppCompatActivity {
 
@@ -10,5 +13,21 @@ public class HouseModifyActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_house_modify);
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                finish();
+                break;
+
+            default:
+                break;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
--- app/src/main/java/com/minerdev/greformanager/MyMenuActivity.java
+++ app/src/main/java/com/minerdev/greformanager/MyMenuActivity.java
@@ -1,8 +1,12 @@
 package com.minerdev.greformanager;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 
 public class MyMenuActivity extends AppCompatActivity {
 
@@ -10,5 +14,22 @@ public class MyMenuActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my_menu);
+
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                finish();
+                break;
+
+            default:
+                break;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
