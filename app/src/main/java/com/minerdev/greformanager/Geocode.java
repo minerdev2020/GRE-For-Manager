abeaf61b3c34809af7d1a5729f8427b5ef3629e6
minerdev <minerdev2020@gmail.com>
2021-02-04 16:11:34 +0900

    파일 정리.

--- app/src/main/java/com/minerdev/greformanager/Geocode.java
+++ app/src/main/java/com/minerdev/greformanager/Geocode.java
@@ -81,48 +81,6 @@ public class Geocode {
         AppHelper.requestQueue.add(request);
     }
 
-//    public void getQueryResponseFromNaver(Context context, String address) {
-//        try {
-//            address = URLEncoder.encode(address, "UTF-8");
-//
-//        } catch (Exception e) {
-//            e.printStackTrace();
-//        }
-//
-//        String apiURL = context.getString(R.string.naver_open_api_geocode);
-//
-//        ContentValues values = new ContentValues();
-//        values.put("query", address);
-//
-//        ContentValues headers = new ContentValues();
-//
-//        String client_id = context.getString(R.string.client_id);
-//        String client_secret = context.getString(R.string.client_secret);
-//
-//        headers.put(context.getString(R.string.api_key_id_header), client_id);
-//        headers.put(context.getString(R.string.api_key_header), client_secret);
-//
-//        NetworkTask networkTask = new NetworkTask(context, apiURL, values, headers);
-//        networkTask.setDialogMsg("좌표를 얻어오는중...");
-//        networkTask.setOnDataReceiveCallback(new NetworkTask.OnDataReceiveCallback() {
-//            @Override
-//            public void parseData(String receivedString) {
-//                try {
-//                    Gson gson = new Gson();
-//                    result = gson.fromJson(receivedString, GeocodeResult.class);
-//
-//                    if (result != null) {
-//                        listener.parseData(result);
-//                    }
-//
-//                } catch (Exception e) {
-//                    e.printStackTrace();
-//                }
-//            }
-//        });
-//        networkTask.execute();
-//    }
-
     public interface OnDataReceiveListener {
         void parseData(GeocodeResult result);
     }
